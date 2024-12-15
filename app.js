a0fa62e978cfb4926f6ac6d79de345d44d9cae48
Rizz-33 <risiniamarathunga@gmail.com>
2024-12-15 19:07:00 +0530

    chore: Port

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require('express');
-const connectToMongoDB = require('./db_connection');
+const express = require("express");
+const connectToMongoDB = require("./db_connection");
 
 const app = express();
-const port = 8085;
+const port = 4000;
 
-connectToMongoDB().then(db => {
-  app.get('/', (req, res) => {
-    res.send('Server is up and running');
-  });
+connectToMongoDB()
+  .then((db) => {
+    app.get("/", (req, res) => {
+      res.send("Server is up and running");
+    });
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed to connect to MongoDB. Server not started.");
   });
-}).catch(error => {
-  console.error("Failed to connect to MongoDB. Server not started.");
-});
