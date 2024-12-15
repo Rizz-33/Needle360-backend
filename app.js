b023c12bc803ab81204de2786bc73771d0f4d6b5
Rizz-33 <risiniamarathunga@gmail.com>
2024-12-15 18:56:33 +0530

    feat: DB conn

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const express = require("express");
+const express = require('express');
+const connectToMongoDB = require('./db_connection');
+
 const app = express();
+const port = 8085;
 
-app.get("/", (req, res) => {
-  res.send("Server is up and running");
-});
+connectToMongoDB().then(db => {
+  app.get('/', (req, res) => {
+    res.send('Server is up and running');
+  });
 
-app.listen(8085, () => {
-  console.log("Server is running on port 8085");
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}).catch(error => {
+  console.error("Failed to connect to MongoDB. Server not started.");
 });
