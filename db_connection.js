e59e8dc8d4f445e6e98b8f02c9442bfa92e34daf
Rizz-33 <risiniamarathunga@gmail.com>
2024-12-21 18:11:51 +0530

    migrate: database connection from MongoClient to Mongoose

--- db_connection.js
+++ db_connection.js
@@ -1,19 +1,29 @@
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
 const connectionString = `mongodb+srv://risiniamarathunga:${password}@devcluster.4xxln.mongodb.net/?retryWrites=true&w=majority&appName=DevCluster`;
 
-const client = new MongoClient(connectionString);
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
 
 export async function connectToMongoDB() {
   try {
-    await client.connect();
-    const db = client.db("Rizz-33");
-    console.log("Connected to MongoDB");
-    return db;
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+    });
+    console.log("Connected to MongoDB with Mongoose");
   } catch (e) {
     console.error("Error connecting to MongoDB:", e.message);
     process.exit(1);
