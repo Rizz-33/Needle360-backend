6ac0e25819506fbee86395333053db1b68c43cd0
Rizz-33 <risiniamarathunga@gmail.com>
2025-01-28 22:41:57 +0530

    refactor: add email sending functionality for verification and welcome emails

---
+++ mailtrap/emails.js
@@ -0,0 +1,62 @@
+import {
+  VERIFICATION_EMAIL_TEMPLATE,
+  WELCOME_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
+import { mailtrapClient, sender } from "./mailtrap.config.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  const recipient = [
+    {
+      email,
+    },
+  ];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Please verify your email address",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    });
+
+    console.log(
+      `Verification email sent successfully to ${email}. Response ID: ${response.id}`
+    );
+  } catch (error) {
+    console.error(
+      `Failed to send verification email to ${email}: ${error.message}`
+    );
+
+    throw new Error(`Failed to send verification email: ${error.message}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [
+    {
+      email,
+    },
+  ];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Welcome to Needle360!",
+      html: WELCOME_EMAIL_TEMPLATE.replace("{username}", name),
+      category: "Welcome Email",
+    });
+
+    console.log(
+      `Welcome email sent successfully to ${email}. Response ID: ${response.id}`
+    );
+  } catch (error) {
+    console.error(`Failed to send welcome email to ${email}: ${error.message}`);
+
+    throw new Error(`Failed to send welcome email: ${error.message}`);
+  }
+};
