a55de217c394bd8b56a0a1b043e312185dc0b8af
Rizz-33 <risiniamarathunga@gmail.com>
2025-01-28 12:37:51 +0530

    refactor: update tailor shop owner schema to include shop details and set collection name

--- models/Schemas/tailor-shop-owners.schema.js
+++ models/Schemas/tailor-shop-owners.schema.js
@@ -2,33 +2,36 @@ import mongoose from "mongoose";
 import ROLES from "../../constants.js";
 import BaseUser from "../base-user.model.js";
 
-const tailorShopOwnerSchema = new mongoose.Schema({
-  role: {
-    type: String,
-    default: ROLES.TAILOR_SHOP_OWNER,
-    required: true,
+const tailorShopOwnerSchema = new mongoose.Schema(
+  {
+    role: {
+      type: String,
+      default: ROLES.TAILOR_SHOP_OWNER,
+      required: true,
+    },
+    shopName: {
+      type: String,
+      required: true,
+    },
+    shopAddress: {
+      type: String,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+    },
+    shopRegistrationNumber: {
+      type: String,
+      required: true,
+    },
+    privileges: {
+      type: [String],
+      default: [],
+    },
   },
-  shopName: {
-    type: String,
-    required: true,
-  },
-  shopAddress: {
-    type: String,
-    required: true,
-  },
-  contactNumber: {
-    type: String,
-    required: true,
-  },
-  shopRegistrationNumber: {
-    type: String,
-    required: true,
-  },
-  privileges: {
-    type: [String],
-    default: [],
-  },
-});
+  { collection: `users.${ROLES.TAILOR_SHOP_OWNER}` }
+);
 
 const TailorShopOwner = BaseUser.discriminator(
   "TailorShopOwner",
