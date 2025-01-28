95315eccf045a801275d3e08a8bc37a6e69b2bd3
Rizz-33 <risiniamarathunga@gmail.com>
2025-01-28 12:38:01 +0530

    refactor: enhance user schema to include contact and bank details, and set collection name

--- models/Schemas/users.schema.js
+++ models/Schemas/users.schema.js
@@ -2,37 +2,40 @@ import mongoose from "mongoose";
 import ROLES from "../../constants.js";
 import BaseUser from "../base-user.model.js";
 
-const userSchema = new mongoose.Schema({
-  role: {
-    type: String,
-    default: ROLES.USER,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    role: {
+      type: String,
+      default: ROLES.USER,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    bankAccountNumber: {
+      type: String,
+      required: true,
+    },
+    bankName: {
+      type: String,
+      required: true,
+    },
+    bankBranch: {
+      type: String,
+      required: true,
+    },
+    privileges: {
+      type: [String],
+      default: [],
+    },
   },
-  contactNumber: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-    required: true,
-  },
-  bankAccountNumber: {
-    type: String,
-    required: true,
-  },
-  bankName: {
-    type: String,
-    required: true,
-  },
-  bankBranch: {
-    type: String,
-    required: true,
-  },
-  privileges: {
-    type: [String],
-    default: [],
-  },
-});
+  { collection: `users.${ROLES.USER}` }
+);
 
 const User = BaseUser.discriminator("User", userSchema);
 
