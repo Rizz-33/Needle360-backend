edfa8bfbc01d327f9b8a2adefe23cef1cf8d61d4
Rizz-33 <risiniamarathunga@gmail.com>
2025-01-28 12:37:35 +0530

    refactor: update admin schema to include privileges and set collection name

--- models/Schemas/admin.schema.js
+++ models/Schemas/admin.schema.js
@@ -2,17 +2,20 @@ import mongoose from "mongoose";
 import ROLES from "../../constants.js";
 import BaseUser from "../base-user.model.js";
 
-const adminSchema = new mongoose.Schema({
-  role: {
-    type: String,
-    default: ROLES.ADMIN,
-    required: true,
+const adminSchema = new mongoose.Schema(
+  {
+    role: {
+      type: String,
+      default: ROLES.ADMIN,
+      required: true,
+    },
+    privileges: {
+      type: [String],
+      default: [],
+    },
   },
-  privileges: {
-    type: [String],
-    default: [],
-  },
-});
+  { collection: `users.${ROLES.ADMIN}` }
+);
 
 const Admin = BaseUser.discriminator("Admin", adminSchema);
 
