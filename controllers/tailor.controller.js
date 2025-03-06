935e3ec2c8c3ac4d64fe546ce7678ff1e815267f
Rizz-33 <risiniamarathunga@gmail.com>
2025-03-06 14:17:04 +0530

    refactor: remove getTailorShopLogos endpoint from controller and route

--- controllers/tailor.controller.js
+++ controllers/tailor.controller.js
@@ -1,27 +1,6 @@
 import mongoose from "mongoose";
 import ROLES from "../constants.js";
 
-export const getTailorShopLogos = async (req, res) => {
-  try {
-    const db = mongoose.connection.db;
-    const shops = await db
-      .collection("users")
-      .find({
-        role: ROLES.TAILOR_SHOP_OWNER,
-        logoUrl: { $exists: true, $ne: null },
-      })
-      .project({ logoUrl: 1, shopName: 1 })
-      .toArray();
-
-    res.json(shops);
-  } catch (error) {
-    console.error("Error fetching logos:", error); // Log the error
-    res
-      .status(500)
-      .json({ message: "Error fetching logos", error: error.message });
-  }
-};
-
 export const getAllTailors = async (req, res) => {
   try {
     const db = mongoose.connection.db;
