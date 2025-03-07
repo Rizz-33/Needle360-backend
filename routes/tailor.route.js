b1949d5d62c6fc31421363bafd88638019b8c32c
Rizz-33 <risiniamarathunga@gmail.com>
2025-03-07 09:55:34 +0530

    feat: add getTailorById endpoint to retrieve tailor details by ID

--- routes/tailor.route.js
+++ routes/tailor.route.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { getAllTailors } from "../controllers/tailor.controller.js";
+import {
+  getAllTailors,
+  getTailorById,
+} from "../controllers/tailor.controller.js";
 
 const router = express.Router();
 
 router.get("/tailors", getAllTailors);
+router.get("/tailors/:id", getTailorById);
 
 export default router;
