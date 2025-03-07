7d086f2810c8a4e976a0ccfa82b57137146e7e69
Rizz-33 <risiniamarathunga@gmail.com>
2025-03-07 12:10:12 +0530

    feat: add updateTailorById endpoint to allow updating tailor details

--- routes/tailor.route.js
+++ routes/tailor.route.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   getAllTailors,
   getTailorById,
+  updateTailorById,
 } from "../controllers/tailor.controller.js";
 
 const router = express.Router();
 
 router.get("/tailors", getAllTailors);
 router.get("/tailors/:id", getTailorById);
+router.put("/tailors/:id", updateTailorById);
 
 export default router;
