ea050c9771829cda3f6fcaf54d6ed7cc18688f15
Rizz-33 <risiniamarathunga@gmail.com>
2025-03-12 14:11:46 +0530

    refactor: remove unapproved tailor fetching methods from controller and routes

--- routes/tailor.route.js
+++ routes/tailor.route.js
@@ -2,8 +2,6 @@ import express from "express";
 import {
   getAllTailors,
   getTailorById,
-  getUnapprovedTailorById,
-  getUnapprovedTailors,
   updateTailorById,
 } from "../controllers/tailor.controller.js";
 
@@ -12,7 +10,5 @@ const router = express.Router();
 router.get("/tailors", getAllTailors);
 router.get("/tailors/:id", getTailorById);
 router.put("/tailors/:id", updateTailorById);
-router.get("/unapproved-tailors", getUnapprovedTailors);
-router.get("/unapproved-tailors/:id", getUnapprovedTailorById);
 
 export default router;
