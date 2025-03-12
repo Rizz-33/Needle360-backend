7c680b599c9e4a2643e9b35fc52fb48297c11d3d
Rizz-33 <risiniamarathunga@gmail.com>
2025-03-12 11:04:20 +0530

    feat: add method and route to fetch unapproved tailor by ID

--- routes/tailor.route.js
+++ routes/tailor.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   getAllTailors,
   getTailorById,
+  getUnapprovedTailorById,
   getUnapprovedTailors,
   updateTailorById,
 } from "../controllers/tailor.controller.js";
@@ -12,5 +13,6 @@ router.get("/tailors", getAllTailors);
 router.get("/tailors/:id", getTailorById);
 router.put("/tailors/:id", updateTailorById);
 router.get("/unapproved-tailors", getUnapprovedTailors);
+router.get("/unapproved-tailors/:id", getUnapprovedTailorById);
 
 export default router;
