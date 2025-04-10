fa774b1cf05a4ff29459b5291975ecca481be056
Rizz-33 <risiniamarathunga@gmail.com>
2025-04-10 20:52:52 +0530

    feat: reorganize design routes for better structure and include customer design operations

--- routes/design.route.js
+++ routes/design.route.js
@@ -1,18 +1,35 @@
 import express from "express";
 import {
+  createCustomerDesign,
   createTailorDesign,
+  deleteCustomerDesign,
   deleteTailorDesign,
+  getAllCustomerDesigns,
+  getAllDesigns,
   getAllTailorDesigns,
+  getCustomerDesignsById,
   getTailorDesignsById,
+  updateCustomerDesign,
   updateTailorDesign,
 } from "../controllers/design.controller.js";
 
 const router = express.Router();
 
-router.get("/", getAllTailorDesigns);
-router.get("/:id", getTailorDesignsById);
-router.post("/:id", createTailorDesign);
-router.put("/:id/designs/:designId", updateTailorDesign);
-router.delete("/:id/designs/:designId", deleteTailorDesign);
+// General routes
+router.get("/", getAllDesigns);
+
+// Tailor-specific routes
+router.get("/tailors", getAllTailorDesigns);
+router.get("/tailors/:id", getTailorDesignsById);
+router.post("/tailors/:id", createTailorDesign);
+router.put("/tailors/:id/designs/:designId", updateTailorDesign);
+router.delete("/tailors/:id/designs/:designId", deleteTailorDesign);
+
+// Customer-specific routes
+router.get("/customers", getAllCustomerDesigns);
+router.get("/customers/:id", getCustomerDesignsById);
+router.post("/customers/:id", createCustomerDesign);
+router.put("/customers/:id/designs/:designId", updateCustomerDesign);
+router.delete("/customers/:id/designs/:designId", deleteCustomerDesign);
 
 export default router;
