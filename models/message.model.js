58b236e471a31b4e80782de407a9f5243e5ea14b
Rizz-33 <risiniamarathunga@gmail.com>
2025-04-12 11:20:57 +0530

    feat: add message model with sender, content, conversation, and attachments schema

---
+++ models/message.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "BaseUser", // Changed from "User" to "BaseUser"
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    conversation: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Conversation",
+      required: true,
+    },
+    readBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "BaseUser", // Changed from "User" to "BaseUser"
+      },
+    ],
+    attachments: [
+      {
+        url: String,
+        type: String,
+      },
+    ],
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    collection: "messages",
+  }
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
