5c37b316fa6386b0563519cbe532068aca3e41b5
Rizz-33 <risiniamarathunga@gmail.com>
2025-04-12 15:07:23 +0530

    feat: extend token expiration to 24 hours and improve cookie settings in generateTokenAndSetCookie

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -1,33 +1,23 @@
-
+// Backend: modified generateTokenAndSetCookie.js
 import jwt from "jsonwebtoken";
 
-
-
 export const generateTokenAndSetCookie = (res, userId) => {
-
   if (!process.env.JWT_SECRET) {
-
     throw new Error("JWT_SECRET is not defined");
-
   }
 
-
-
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-
-    expiresIn: "1h",
-
+    expiresIn: "24h", // Extended for convenience while testing
   });
 
-
-
+  // Set HTTP-only cookie
   res.cookie("token", token, {
-
     httpOnly: true,
-
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict", // csrf protection
-
+    sameSite: "strict",
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
   });
 
+  // Return token to include in response body
+  return token;
 };
