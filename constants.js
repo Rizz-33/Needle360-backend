e1d728c3cd530f26160e8dd948c3233c5a3bf646
Rizz-33 <risiniamarathunga@gmail.com>
2025-04-17 12:19:10 +0530

    feat: add predefined services list to constants

--- constants.js
+++ constants.js
@@ -5,3 +5,19 @@ const ROLES = {
 };
 
 export default ROLES;
+
+// Predefined services list
+const PREDEFINED_SERVICES = [
+  "School Uniforms",
+  "Saree Blouses",
+  "Wedding Attire",
+  "Office Wear",
+  "National Dress",
+  "Formal Wear",
+  "Casual Wear",
+  "Kidswear",
+  "Religious/Cultural Outfits",
+  "Custom Fashion Designs",
+];
+
+export { PREDEFINED_SERVICES };
